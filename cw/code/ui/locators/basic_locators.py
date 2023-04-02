42caeecb5746b68eafd6d8e3015143a9d0c2c8d2
“Neumoin <d.neumoin@vk.team>
2023-04-02 23:59:30 +0300

    add classwork

---
+++ cw/code/ui/locators/basic_locators.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    QUERY_LOCATOR = (By.NAME, 'q')
+    QUERY_LOCATOR_ID = (By.ID, 'id-search-field')
+    GO_BUTTON_LOCATOR = (By.XPATH, '//*[@id="submit"]')
+    START_SHELL = (By.ID, 'start-shell')
+    PYTHON_CONSOLE = (By.ID, 'hterm:row-nodes')
+
+
+class MainPageLocators(BasePageLocators):
+    COMPREHENSIONS = (
+        By.XPATH,
+        '//code/span[@class="comment" and contains(text(), "comprehensions")]'
+    )
+    EVENTS = (By.ID, 'events')
+    READ_MORE = (By.CSS_SELECTOR, 'a.readmore')
+    PROFILE_LOCATOR = (By.XPATH, "//a[contains(@class, 'center-module-profile')]")
+    TOOLS_LOCATOR = (By.XPATH, "//a[contains(@class, 'center-module-tools')]")
+
+
+class EventsPageLocators(BasePageLocators):
+    pass
